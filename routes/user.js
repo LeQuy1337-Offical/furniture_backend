a64ea4dca164eabffe7d6c95ae48e02bf6717e64
John Le <quylp10dev@gmail.com>
2024-02-19 23:35:57 +0700

    first commit

---
+++ routes/user.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const userController = require('../controllers/userControllers');
+
+
+
+router.delete('/:id', userController.deleteUser);
+router.get('/:id', userController.getUser);
+
+
+module.exports = router;
